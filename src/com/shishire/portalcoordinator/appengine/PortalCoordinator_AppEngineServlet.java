982f5e639096df39d452e1382fd7c35f0cfd173d
Wesley Hirsch <emperorshishire@gmail.com>
2013-03-30 20:02:29 -0400

    Fixed Eclipse issues involving classpaths

--- src/com/shishire/portalcoordinator/appengine/PortalCoordinator_AppEngineServlet.java
+++ src/com/shishire/portalcoordinator/appengine/PortalCoordinator_AppEngineServlet.java
@@ -4,10 +4,11 @@ import java.io.IOException;
 import javax.servlet.http.*;
 
 @SuppressWarnings("serial")
-public class PortalCoordinator_AppEngineServlet extends HttpServlet {
-	public void doGet(HttpServletRequest req, HttpServletResponse resp)
-			throws IOException {
-		resp.setContentType("application/json");
-		resp.getWriter().println("{ \"Shishire\" : [ { \"portal-name\" : \"Cooper's Tavern\", \"deploy-octant\" : \"NE\" }, { \"portal-name\" : \"Reagent Theater\", \"deploy-octant\" : \"SW\" }  ] }");
+public class PortalCoordinator_AppEngineServlet extends HttpServlet
+{
+	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException
+	{
+		resp.setContentType("text/plain");
+		resp.getWriter().println("Hello, world");
 	}
 }
---
+++ src/com/shishire/portalcoordinator/appengine/RESTAPI.java
@@ -0,0 +1,9 @@
+package com.shishire.portalcoordinator.appengine;
+
+import javax.ws.rs.Path;
+
+@Path("/1")
+public class RESTAPI
+{
+
+}
